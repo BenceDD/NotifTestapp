da778a7a07a4af92f73c894f5ed3e23d22d507b1
Mikulás Bence <mikulasbence@gmail.com>
2020-10-21 14:00:18 +0200

    Initial commit

---
+++ NotificationTestApp/NotificationTestApp/ViewModels/DataGridViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationTestApp.ViewModels {
+
+    public class DataGridItemViewModel {
+        public Dictionary<string, string> Properties { 
+            set; 
+            get;
+        }
+    }
+
+
+    public class DataGridViewModel {
+        public ObservableCollection<Dictionary<string, string>> DataStore { set; get; } = new ObservableCollection<Dictionary<string, string>>();
+
+        public DataGridViewModel() {
+            DataStore.Add(new Dictionary<string, string>() { { "column1", "korte" }, { "column2", "nyuszi" } });
+            DataStore.Add(new Dictionary<string, string>() { { "column1", "nyamm" }, { "puding3", "boci" } });
+            DataStore.Add(new Dictionary<string, string>() { { "column2", "hohoho" }, { "puding3", "trololo" } });
+        }
+    }
+}
---
+++ NotificationTestApp/NotificationTestApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using NotificationTestApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationTestApp {
+    public class MainWindowViewModel {
+    }
+}
